4995694018802facd8051addabc7506a657e1f06
Chris Kuech <cjkuech@gmail.com>
2019-11-17 13:14:28 -0800

    show namespace in requirement events

--- src/types.ps1
+++ src/types.ps1
@@ -24,12 +24,14 @@ enum LifecycleState {
 
 class RequirementEvent {
     [datetime] $Date
+    [string] $Namespace
     [Method] $Method
     [LifecycleState] $State
     [object] $Result
     [Requirement] $Requirement
     hidden Init([Requirement]$Requirement, [Method]$Method, [LifecycleState]$State, $Result) {
         $this.Date = Get-Date
+        $this.Namespace = $Requirement.Namespace
         $this.Method = $Method
         $this.State = $State
         $this.Result = $Result
