c187d72133c5d1a70d9250b4f0cc3254b84e157e
Chris Kuech <chkuech@microsoft.com>
2019-11-26 20:07:43 -0800

    test => false throws

--- src/core.ps1
+++ src/core.ps1
@@ -9,24 +9,34 @@ $NamespaceDelimiter = ":"
 
 # idempotently applies a requirement
 function applyRequirement([Requirement]$Requirement) {
-  $result = $false
-  if ($Requirement.Test) {
-    [RequirementEvent]::new($Requirement, "Test", "Start")
-    $result = &$Requirement.Test
-    [RequirementEvent]::new($Requirement, "Test", "Stop", $result)
-  }
-  if (-not $result) {
-    if ($Requirement.Set) {
-      [RequirementEvent]::new($Requirement, "Set", "Start")
-      $result = &$Requirement.Set
-      [RequirementEvent]::new($Requirement, "Set", "Stop", $result)
-    }
-    if ($Requirement.Test -and $Requirement.Set) {
-      [RequirementEvent]::new($Requirement, "Validate", "Start")
-      $result = &$Requirement.Test
-      [RequirementEvent]::new($Requirement, "Validate", "Stop", $result)
-      if (-not $result) {
+  switch (("Test", "Set" | ? { $Requirement.$_ }) -join "-") {
+    "Test" {
+      [RequirementEvent]::new($Requirement, "Test", "Start")
+      $testResult = &$Requirement.Test
+      [RequirementEvent]::new($Requirement, "Test", "Stop", $testResult)
+      if (-not $testResult) {
         Write-Error "Failed to apply Requirement '$($Requirement.Describe)'"
+      }
+    }
+    "Set" {
+      [RequirementEvent]::new($Requirement, "Set", "Start")
+      $setResult = &$Requirement.Set
+      [RequirementEvent]::new($Requirement, "Set", "Stop", $setResult)
+    }
+    "Test-Set" {
+      [RequirementEvent]::new($Requirement, "Test", "Start")
+      $testResult = &$Requirement.Test
+      [RequirementEvent]::new($Requirement, "Test", "Stop", $testResult)
+      if (-not $testResult) {
+        [RequirementEvent]::new($Requirement, "Set", "Start")
+        $setResult = &$Requirement.Set
+        [RequirementEvent]::new($Requirement, "Set", "Stop", $setResult)
+        [RequirementEvent]::new($Requirement, "Validate", "Start")
+        $validateResult = &$Requirement.Test
+        [RequirementEvent]::new($Requirement, "Validate", "Stop", $validateResult)
+        if (-not $validateResult) {
+          Write-Error "Failed to apply Requirement '$($Requirement.Describe)'"
+        }
       }
     }
   }
@@ -55,8 +65,7 @@ function testRequirements([Requirement[]]$Requirements) {
 function sortRequirements([Requirement[]]$Requirements) {
   $stages = @()
   while ($Requirements) {
-    $nextStages = $Requirements `
-    | ? { -not ($_.DependsOn | ? { $_ -notin $stages.Namespace }) }
+    $nextStages = $Requirements | ? { -not ($_.DependsOn | ? { $_ -notin $stages.Namespace }) }
     if (-not $nextStages) {
       throw "Could not resolve the dependencies for Requirements with names: $($Requirements.Namespace -join ', ')"
     }
